25d86b0b9c80e6038ec44164e7ff497028024253
Kato <katuninecm@gmail.com>
2023-02-26 16:00:24 +0100

    Made a function to play one round of game

--- javascript.js
+++ javascript.js
@@ -3,12 +3,49 @@ function getComputerChoice(){
 
     if(choice == 0){
         let result = "rock";
-        console.log(result);
+        return result;
     }else if(choice == 1){
         let result = "paper";
-        console.log(result);
+        return result;
     }else{
         let result = "scissors";
-        console.log(result);
+        return result;
     }
 }
+
+function playRound(){
+    let playerChoice = prompt("Please choose between rock, paper or scissors");
+    let computerChoice = getComputerChoice();
+
+    playerChoice = playerChoice.toLowerCase();
+    
+    if(computerChoice == "rock"){
+        if(playerChoice == "rock"){
+            console.log("It's a draw! Rock and Rock.")
+        }else if(playerChoice == ("paper")){
+            console.log("Yes! You won. Paper beats Rock.")
+        }else{
+            console.log("You lose! Rock beats scissors.")
+        }
+
+    }else if(computerChoice == "paper"){
+        if(playerChoice == "rock"){
+            console.log("You lose! Paper beats rock.")
+        }else if(playerChoice == "paper"){
+            console.log("It's a draw! Paper and Paper.")
+        }else{
+            console.log("You win! Scissors beat Paper.")
+        }
+    }else{
+        if(playerChoice == "rock"){
+            console.log("You win! Rock beats Scissors.")
+
+        }else if(playerChoice == "paper"){
+            console.log("You lose! Scissors beat Paper.")
+
+        }else{
+                console.log("It's a draw. Scissors and Scissors.")
+            }
+        }
+    }
+

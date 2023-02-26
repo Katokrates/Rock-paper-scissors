df5619c28984ab23ff82c0fa77e2c2b446f0bf00
Kato <katuninecm@gmail.com>
2023-02-26 15:43:36 +0100

    Function now prints the choice the PC made

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
+
+    if(choice == 0){
+        let result = "rock";
+        console.log(result);
+    }else if(choice == 1){
+        let result = "paper";
+        console.log(result);
+    }else{
+        let result = "scissors";
+        console.log(result);
+    }
 }
